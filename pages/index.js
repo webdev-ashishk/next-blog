351e74e1ed94397ccddd05e42034df21b146d5f6
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-09 15:47:18 +0530

    initial navbar ready

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h2>HOme</h2>
+    </div>
+  );
+}
